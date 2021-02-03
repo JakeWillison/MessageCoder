91bd08f4b6ff8e8f6fa7b42a8e47cd1df390e7c3
Jake Willison <jakewillison23@gmail.com>
2021-02-02 22:28:44 -0500

    Added some functions to scramble the alphabet and button functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,43 @@ using System.Windows.Shapes;
 
 namespace MessageCoder
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private char[] alpha = new char[26];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = MessageBox.Text.Trim();
+            int scrambleNum = int.Parse(ScrambleNumBox.Text.Trim());
+            Rearrange(scrambleNum, alpha);
+            string newMessage = Scramble(message, alpha);
+        }
+        public void Rearrange(int n, char[] alpha)  //This function rearranges the alphabet based on the number entered.
+        {
+            int timesRun = 0;
+            do
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    char temp = alpha[i];
+                    alpha[i] = alpha[i + i];
+                    alpha[i + i] = temp;
+                }
+                timesRun++;
+            } while (timesRun < n);
+        }
+
+        string Scramble(string message, char[] alpha)
+        {
+            //find letter in new alphabet
+            //perform ceasar sypher
+            //replace letter
+            return "temp return";
         }
     }
 }
